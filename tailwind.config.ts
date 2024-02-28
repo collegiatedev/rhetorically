5854be238020744469dc917249551127d2f6650c
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-02-28 00:47:55 -0800

    import collegiate colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cc-black": "#121212",
+        "cc-blue-dark": "#0A1931",
+        "cc-blue-light": "#9FBFEB",
+        "cc-blue": "#122C59",
+        "cc-blue-highlight": "#185ADB",
+        "cc-white": "#F2F2F2",
+        "cc-yellow": "#FFC947",
+        "cc-orange": "#D9882B",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
